bbe63e5b7edfee33ed1a661d43c749ce992de519
Paulo Fernando Hartelt Neves <paulohartelt@hotmail.com>
2022-12-07 11:25:51 -0300

    adicionado registro de aulas + obs

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -1,3 +1,25 @@
-/*Aqui são os controladores dos agendamentos de aula*/
+/*Aqui são os controladores dos agendamentos de aula
+- Precisa ainda pegar o id_professor utilizando o token de autenticação.
+- Adicionar uma trava no para_quando, para se houver outras aulas marcadas. 
+*/
 
 const connection = require('../connection');
+
+const registerSchedule = async (req, res) => {
+  const { turma, quantidade_alunos, quando_marcado, para_quando } = req.body;
+  try {
+    const { rowCount, rows } = await connection.query(
+      'INSERT INTO agendamento_aulas (turma, quantidade_alunos, quando_marcado, para_quando) VALUES ($1, $2, $3) RETURNING *',
+      [turma, quantidade_alunos, quando_marcado, para_quando]
+    );
+    if (rowCount === 0) {
+      return res
+        .status(400)
+        .json({ message: 'Não foi possível registrar o usuário' });
+    }
+    const user = rows[0];
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
